70020ea5df55e70a43a936a48c0bd48076a6205b
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-01-14 18:18:18 +0300

    Add files via upload

---
+++ 6/CompPract_6/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompPract_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача #1
+            //Console.Write("Введите ваше число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Следующее за числом {num} число – {num + 1}.");
+            //Console.WriteLine($"Для числа {num} предыдущее число – {num - 1}.");
+
+            // Задача #2 
+            //string a = "забыть";
+            //string b = "чисто";
+            //string c = "на";
+            //string d = "всегда";
+            //string e = "и";
+            //string x = string.Concat(a," ", c, b, " ", e, " ", c, d);            
+            //Console.WriteLine(x);
+
+            // Задача #3
+            //Console.Write("Введите первое слово: ");
+            //string a = Console.ReadLine();
+            //Console.Write("Введите второе слово: ");
+            //string b = Console.ReadLine();
+            //Console.Write("Введите третье слово: ");
+            //string c = Console.ReadLine();
+            //Console.Write("Введите четвертое слово: ");
+            //string d = Console.ReadLine();
+            //Console.Write("Введите пятое слово: ");
+            //string e = Console.ReadLine();
+            //Console.WriteLine($"{a}\n{b}");
+            //Console.WriteLine($"{c}");
+            //Console.WriteLine($"{d}\n{e}");
+
+            //// Задача #4 (Одна команда имеет 20 пробелов)
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("         ###########"); // Верхнее основание парал.
+            //Console.WriteLine("        ########### ");
+            //Console.WriteLine("       ###########  ");
+            //Console.WriteLine("      ###########   ");
+            //Console.WriteLine("     ###########    ");
+            //Console.WriteLine("    ###########     ");
+            //Console.WriteLine("   ###########      ");
+            //Console.WriteLine("  ###########       ");
+            //Console.WriteLine(" ###########        ");
+            //Console.WriteLine("###########         "); // Нижнее основание парал.
+            //Console.WriteLine("                    ");
+            //Console.WriteLine("                    ");
+
+            // Задача #5
+            //Console.WriteLine("           "); //7
+            //Console.WriteLine("           ");
+            //Console.WriteLine("       ##### "); // Верхняя часть числа
+            //Console.WriteLine("      #     #");
+            //Console.WriteLine("      #     #");
+            //Console.WriteLine("      #     #");
+            //Console.WriteLine("            #");
+            //Console.WriteLine("           # ");
+            //Console.WriteLine("          #  ");
+            //Console.WriteLine("         #    ");
+            //Console.WriteLine("        #    ");
+            //Console.WriteLine("       #     ");
+            //Console.WriteLine("      #      ");
+            //Console.WriteLine("     #       ");
+            //Console.WriteLine("     #######");
+            //Console.WriteLine("           ");
+            Console.Read();
+
+        }
+    }
+}
